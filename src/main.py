8dc5e474254651dc85d8f869be0dc3f29a311dd9
chr1s-b <cboddy1@hotmail.com>
2018-08-17 13:39:58 +0100

    Starting 3D

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 from pathtracing import *
 from line_tracing import *
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def main():
     print("Starting")
     # config variables
     config = {"radii": (100, 500, 1000),
-              "tolerance": 1000,
+              "tolerance": 100,
               "road": 70,
               "acc": 2,
-              "point sets": 1000000,
+              "point sets": 100000,
               "min radius": 1000,
-              "sideroad": 1}
+              "sideroad": 1,
+              "3D": False}
     print("Generating data")
     data = realdata_xyz(config["point sets"])
     print("Generated {} sets of points".format(len(data)))
@@ -40,6 +42,20 @@ def main():
     #plt.subplot(200)
     plt.figure(2)
     side_view(sidexy, sidemc)
+    
+    #3D
+    if config["3D"]:
+        print("Rendering 3D point scatter")
+        fig = plt.figure(3)
+        ax = fig.add_subplot(111,projection='3d')
+        for trio in data:
+            for point in trio:
+                x, y, z = point
+                r = sqrt(x**2 + y**2 + z**2)
+                c = hsv_to_rgb((r/5200., 1, 1))
+                ax.plot([x], [z], [y], 'r+', color=c, alpha=0.7)
+        #ax.set_axis_off()      
+    
     plt.show()
     return
 
